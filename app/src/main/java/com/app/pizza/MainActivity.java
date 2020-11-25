eac6f92341bf8c194eea51c49722965ff28e3755
Adrian <adrian.jaczynski@gmail.com>
2020-11-25 13:55:13 +0100

    login validation

--- app/src/main/java/com/app/pizza/MainActivity.java
+++ app/src/main/java/com/app/pizza/MainActivity.java
@@ -2,10 +2,10 @@ package com.app.pizza;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +15,11 @@ import com.app.pizza.model.UserLogin;
 import com.app.pizza.service.ServiceGenerator;
 import com.app.pizza.service.UserService;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     UserService userService;
     TextInputEditText loginInput;
     TextInputEditText passwordInput;
+    TextInputLayout loginLabel;
+    TextInputLayout passwordLabel;
     TextView textView;
 
     @Override
@@ -32,12 +39,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         userService = ServiceGenerator.createService(UserService.class);
 
         loginInput = findViewById(R.id.loginUsername);
         passwordInput = findViewById(R.id.loginPassword);
         textView = findViewById(R.id.errorTextLogin);
+        loginLabel = findViewById(R.id.loginUsernameLabel);
+        passwordLabel = findViewById(R.id.loginPasswordLabel);
+        textView.setVisibility(TextView.INVISIBLE);
 
         findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
                 call.enqueue(new Callback<UserAuthResponse>() {
                     @Override
                     public void onResponse(Call<UserAuthResponse> call, Response<UserAuthResponse> response) {
+                        textView.setVisibility(TextView.INVISIBLE);
+                        loginLabel.setErrorEnabled(false);
+                        passwordLabel.setErrorEnabled(false);
+
                         if(response.isSuccessful()){
                             Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
                             Log.d("string",response.body().toString());
@@ -62,22 +75,27 @@ public class MainActivity extends AppCompatActivity {
 
                             Log.d("Rola", userAuthResponse.getRoles().toString());
                             startActivity(intent);
-//                            if(userAuthResponse.getRoles().toString().contains("ROLE_ADMIN")){
-//                                startActivity(intent);
-//                            }
-//                            else
-//                            {
-//                                textView.setText("Brak uprawnien administratora");
-//                                textView.setVisibility(TextView.VISIBLE);
-//                            }
 
                         }
-                        else
-                    {
-                        textView.setText("Zle dane");
-                        textView.setVisibility(TextView.VISIBLE);
-                        Log.d("ERROR","ERROR");
-                    }
+                        else if(response.code() == 400){
+                            Gson gson = new GsonBuilder().create();
+                            UserLogin mError;
+                            try {
+                                mError= gson.fromJson(response.errorBody().string(), UserLogin.class);
+                                loginLabel.setError(mError.getUsername());
+                                passwordLabel.setError(mError.getPassword());
+                                loginLabel.setErrorEnabled(true);
+                                passwordLabel.setErrorEnabled(true);
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        else if(response.code() == 401)
+                        {
+                            textView.setVisibility(TextView.VISIBLE);
+                            textView.setTextColor(Color.RED);
+                        }
                     }
                     @Override
                     public void onFailure(Call<UserAuthResponse> call, Throwable t) {
